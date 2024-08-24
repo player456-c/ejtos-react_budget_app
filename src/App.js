20a0185f16b6934196da8dd9816b9f7904f6fbdf
Call me Fuckface, Vasco <veron.vbn1234@gmail.com>
2024-08-24 06:44:13 -0400

    4'th, right before final project

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Budget from './components/Budget';
 import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
-import ExpenseItem from './components/ExpenseItem';
+//import ExpenseItem from './components/ExpenseItem';
 import AllocationForm from './components/AllocationForm';
 
 
@@ -20,49 +20,28 @@ const App = () => {
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
-                        {
-                            // Budget component
-                            <div className='col-sm'>
-                                <Budget />
-                            </div>
-                        }        
-
-                        {
-                            //Remaining component
-                            <div className='col-sm'>
-                                <Remaining />
-                            </div>
-                        }        
-
-                        {
-                            //ExpenseTotal component
-                            <div className='col-sm'>
-                                <ExpenseTotal />
-                            </div>
-                        }        
-                       
-                        {
-                            //ExpenseList component
-                            <div className='col-sm'>
-                                <ExpenseList />
-                            </div>
-                        }         
-
-                        {
-                            //ExpenseItem component
-                            <div className='col-sm'>
-                                <ExpenseItem />
-                            </div>
-                        }        
-
-                        {
-                            //AllocationForm component
-                            <div className='col-sm'>
-                                <AllocationForm />
-                            </div>
-                        }        
-
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <Budget />
+                    </div>
+                    <div className='col-sm'>
+                        <Remaining />
+                    </div>
+                    <div className='col-sm'>
+                        <ExpenseTotal />
+                    </div>
+                </div>
+                <h3 className='mt-3'>Allocation</h3>
+                <div className='row '>
+                    <div className='col-sm'>
+                        <ExpenseList />
+                    </div>
+                </div>
+                <h3 className='mt-3'>Change allocation</h3>
+                <div className='row mt-3'>
+                    <div className='col-sm'>
+                        <AllocationForm/>
+                    </div>
                 </div>
             </div>
         </AppProvider>
